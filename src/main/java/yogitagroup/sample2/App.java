a90c1ffe6496bd877059c29682480f532ae9d724
anujyog1 <agyogita@gmail.com>
2022-05-03 11:44:52 +0530

    First commit

---
+++ src/main/java/yogitagroup/sample2/App.java
@@ -0,0 +1,13 @@
+package yogitagroup.sample2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sample maven web  project  2  " );
+    }
+}
